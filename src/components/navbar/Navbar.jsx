5ab2074ce9b7174ea0f54553f1e9892c680d87dd
Lakshya Garg <lakshya0418.be20@chitkara.edu.in>
2022-09-18 00:22:35 +0530

    Header Completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">Book-With-Us</span>
+        <span className="logo">YATRIGAN</span>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
