f3bfab5ef59bedf7a161ec3c39e12453683b11c3
Lakshya Garg <lakshya0418.be20@chitkara.edu.in>
2022-09-18 14:36:24 +0530

    Navbar Button Changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -44,7 +44,7 @@ const Footer = () => {
           <li className="fListItem">Terms & conditions</li>
         </ul>
       </div>
-      <div className="fText">Copyright © 2022  YATRIGAN.</div>
+      <div className="fText">Copyright © 2022  YAATRIGAN.</div>
     </div>
   );
 };
