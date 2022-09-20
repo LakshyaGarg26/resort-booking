e9ad3d928aad2f3ccb4da79aeefdeaf807ed6287
Lakshya Garg <lakshya0418.be20@chitkara.edu.in>
2022-09-20 07:57:13 +0530

    Asset Folder Added

--- src/components/propertyList/PropertyList.jsx
+++ src/components/propertyList/PropertyList.jsx
@@ -1,11 +1,16 @@
 import "./propertyList.css";
+import Apartments from "../../assets/Apartments.jpg"
+import Cabins from "../../assets/Cabins.jpg"
+import Hotels from "../../assets/Hotels.webp"
+import Resorts from "../../assets/Resorts.jpg"
+import Villas from "../../assets/Villas.jpg"
 
 const PropertyList = () => {
   return (
     <div className="pList">
       <div className="pListItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o="
+          src={Hotels}
           alt=""
           className="pListImg"
         />
@@ -16,7 +21,7 @@ const PropertyList = () => {
       </div>
       <div className="pListItem">
         <img
-          src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg"
+          src={Apartments}
           alt=""
           className="pListImg"
         />
@@ -27,7 +32,7 @@ const PropertyList = () => {
       </div>
       <div className="pListItem">
         <img
-          src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_resorts/6f87c6143fbd51a0bb5d15ca3b9cf84211ab0884.jpg"
+          src={Resorts}
           alt=""
           className="pListImg"
         />
@@ -38,7 +43,7 @@ const PropertyList = () => {
       </div>
       <div className="pListItem">
         <img
-          src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg"
+          src={Villas}
           alt=""
           className="pListImg"
         />
@@ -49,7 +54,7 @@ const PropertyList = () => {
       </div>
       <div className="pListItem">
         <img
-          src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg"
+          src={Cabins}
           alt=""
           className="pListImg"
         />
--- src/components/searchItem/SearchItem.jsx
+++ src/components/searchItem/SearchItem.jsx
@@ -1,5 +1,6 @@
 import "./searchItem.css";
 import { useNavigate } from "react-router-dom";
+import TowerStreetApartments from "../../assets/Tower Street Apartments.webp"
 
 const SearchItem = () => {
 
@@ -11,7 +12,7 @@ const SearchItem = () => {
   return (
     <div className="searchItem">
       <img
-        src="https://cf.bstatic.com/xdata/images/hotel/square600/261707778.webp?k=fa6b6128468ec15e81f7d076b6f2473fa3a80c255582f155cae35f9edbffdd78&o=&s=1"
+        src={TowerStreetApartments}
         alt=""
         className="siImg"
       />
